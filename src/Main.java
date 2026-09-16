8138a546edfd3cdc6aace9fdc65fb9b04e394bff
davidkac <kats10102008@gmail.com>
2026-09-16 16:22:51 +0200

    4 Lesson

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,23 @@
 public class Main {
 
     public static void main(String[] args) {
-        int i = 4;
-        i++;
-        System.out.println(i);
+        int i1 = 3;
+        int i2 = 4;
 
-        int j = 3;
-        System.out.println(i % j);
-
-        System.out.println(i == j);
-
-        System.out.println(!(i == j));
-
-        System.out.println((i + j) < 10 && i*j > 100);
-
-        System.out.println((i + j) < 10 || i*j > 100);
+        Print("Число " + IsEven(i1));
+        Print("Число " + IsEven(i2));
+        PrintDefault();
     }
 
+     static boolean IsEven(int number) {
+        return (number % 2) == 0;
+    }
+
+    static void Print(String text){
+        System.out.println(text);
+    }
+
+    static void PrintDefault() {
+        System.out.println("Изучаем методы");
+    }
 }
