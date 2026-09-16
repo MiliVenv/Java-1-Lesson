dc398036634f2c8ffec25ab8ffbad12387c97634
davidkac <kats10102008@gmail.com>
2026-09-16 15:54:08 +0200

    2 Lesson

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        byte box1 = 1; // 1
+        short box2 = 4; // 2
+        int box3 = 4; // 4
+        long box4 = 8; // 8
+        char box5 = 'a'; // 2
+        boolean box6 = true; // 1
+        float box7 = 44.4f; // 4
+        double box8 = 88.8; // 8
+
+        box3 = box1;
+
+        System.out.println(box3);
+
+        box3 = (int)box4;
+
+        System.out.println(box3);
     }
 
 }
