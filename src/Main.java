55c92fdb9ea3d609b26bbc62c95330d202057087
davidkac <kats10102008@gmail.com>
2026-09-16 16:12:16 +0200

    3 Lesson

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-        byte box1 = 1; // 1
-        short box2 = 4; // 2
-        int box3 = 4; // 4
-        long box4 = 8; // 8
-        char box5 = 'a'; // 2
-        boolean box6 = true; // 1
-        float box7 = 44.4f; // 4
-        double box8 = 88.8; // 8
+        int i = 4;
+        i++;
+        System.out.println(i);
 
-        box3 = box1;
+        int j = 3;
+        System.out.println(i % j);
 
-        System.out.println(box3);
+        System.out.println(i == j);
 
-        box3 = (int)box4;
+        System.out.println(!(i == j));
 
-        System.out.println(box3);
+        System.out.println((i + j) < 10 && i*j > 100);
+
+        System.out.println((i + j) < 10 || i*j > 100);
     }
 
 }
