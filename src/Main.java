86fe2acc1cafe10629d80758b2d18f9e754c8806
davidkac <kats10102008@gmail.com>
2026-09-18 01:49:58 +0200

    7 Lesson

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,43 @@
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        cycleTest();
-    }
+        int[] array = new int[10];
+        Random random = new Random();
 
-    static void cycleTest() {
-        for(int i = 0; i<= 8; i++) {
-            if (i == 3) {
-                continue;
-            }
-            System.out.println(i);
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(20);
         }
+
+        System.out.println(Arrays.toString(array));
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] += 100;
+        }
+
+        System.out.println(Arrays.toString(array));
+
+        for (int element : array) {
+            System.out.println(element % 100);
+        }
+
+        Arrays.sort(array);
+
+        System.out.println(Arrays.toString(array));
+
+        int maxNumber = array[0];
+        for (int element : array) {
+            if (maxNumber < element) maxNumber = element;
+        }
+        System.out.println(maxNumber);
+
+        int sumNumber = 0;
+        for (int element : array) {
+            sumNumber += element;
+        }
+        System.out.println(sumNumber);
     }
 }
